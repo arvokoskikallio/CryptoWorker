707fb1b3755661703960abc816e0c4759d94a515
Arvo Koskikallio <herra.persian@gmail.com>
2021-11-09 13:19:56 +0200

    API sync

--- worker.py
+++ worker.py
@@ -1,5 +1,7 @@
-import csv
 import pandas as pd
+import requests
+import json
+from datetime import datetime
 from quantaworker import Column, ImportWorker, ValueType, DataSample, InvocationParam, List, Parameter, ImportWorker
 
 class csvWorker(ImportWorker):
@@ -10,7 +12,7 @@ class csvWorker(ImportWorker):
 
     @property
     def description(self) -> str:
-        return "csv Test worker"
+        return "api test 9"
 
     @property
     def worker_type(self):
@@ -28,6 +30,8 @@ class csvWorker(ImportWorker):
         column_list.append(Column.output("high", "high", ValueType.float_column(), index=2))
         column_list.append(Column.output("low", "low", ValueType.float_column(), index=3))
         column_list.append(Column.output("close", "close", ValueType.float_column(), index=4))
+        column_list.append(Column.output("tradingvolumebtc", "tradingvolumebtc", ValueType.float_column(), index=5))
+        column_list.append(Column.output("tradingvolumeusd", "tradingvolumeusd", ValueType.float_column(), index=6))
 
         return column_list
 
@@ -42,19 +46,60 @@ class csvWorker(ImportWorker):
                 "high":i,
                 "low":i,
                 "close":i,
+                "tradingvolumebtc":i,
+                "tradingvolumeusd":i
             }
             dict_list.append(temp)
 
         return DataSample(self.columns, pd.DataFrame(data=dict_list))
 
     def run_import(self, params: List[InvocationParam]) -> pd.DataFrame:
-        file = open('btc.csv')
-        csvreader = csv.reader(file)
+        requestPair = "btcusd"  # btcusd, btceur, ethusd, adausd, xrpusd, etc
+        timeScale = "86400"     # set the timescale, see table below
 
-        rows = []
-        for row in csvreader:
-            rows.append(row)
+        # Different timescales in seconds
+        # 60        1  minute
+        # 180       3  minutes
+        # 300       5  minutes
+        # 900       15 minutes
+        # 1800      30 minutes
+        # 3600      1  hour
+        # 7200      2  hours
+        # 14400     4  hours
+        # 21600     6  hours
+        # 43200     12 hours
+        # 86400     1  day
+        # 259200    3  days
+        # 604800    1  week
 
-        print(rows)
+        requestUrl = "https://api.cryptowat.ch/markets/bitfinex/" + requestPair + "/ohlc"
 
-        return pd.DataFrame(columns=rows[0], data=rows[1:])
+        url = requests.get(requestUrl)
+        text = url.text
+
+        # takes the data in, and removes the "result" part of the data, i.e. the first column of the array
+        data = json.loads(text)["result"]
+
+
+        # Print all of the info
+
+        columns = ["time", "open", "high", "low", "close", "tradingvolumebtc", "tradingvolumeusd"]
+        allData = []
+
+        i = 0
+        j = 0
+        while i < len(data[timeScale]):
+            row=[]
+            while j < len(data[timeScale][i]):
+                if j == 0:
+                    row.append(datetime.utcfromtimestamp(data[timeScale][i][j]).strftime('%Y-%m-%dT%H:%M:%SZ'))
+                else:    
+                    row.append(str(data[timeScale][i][j]))
+                j = j + 1
+            allData.append(row)
+            j = 0
+            i = i + 1
+
+        print(allData)
+
+        return pd.DataFrame(columns=columns, data=allData)
